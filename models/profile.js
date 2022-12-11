b61bd9bc07c956a7b3131a2860b1363e00e4b366
kangcarol <otherhats@gmail.com>
2022-12-11 13:07:30 -0500

    add to the card model 'about'

--- models/profile.js
+++ models/profile.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   name: String,
   photo: String,
+  about: String,
   cards: [{ type: Schema.Types.ObjectId, ref: 'Card'}]
 },{
   timestamps: true,
